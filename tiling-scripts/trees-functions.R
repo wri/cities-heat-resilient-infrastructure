3c03c39e1151dfb8f40dd70efba70ce5a8684ade
Elizabeth Jane Wesley <elizabeth.wesley@wri.org>
2025-12-23 10:50:42 -0500

    changed file suffix

--- tiling-scripts/trees-functions.R
+++ tiling-scripts/trees-functions.R
@@ -8,55 +8,59 @@ library(glue)
 library(geoarrow)
 library(sfarrow)
 
-# Functions 
 
-source(here("tiling-scripts", "utils.R"))
+# # Specify variables -------------------------------------------------------
+# 
+# city <- "ZAF-Durban"
+# aoi_name <- "inner_city_lap"
+# 
+# infra_name <- "trees"
+# scenario_name <- "pedestrian-achievable-90pctl"
+# 
+# # Functions 
+# 
+# source(here("tiling-scripts", "utils.R"))
 
-# Sign in to AWS ----------------------------------------------------------
+# # Sign in to AWS ----------------------------------------------------------
+# 
+# system("aws sso login --profile cities-data-dev")
+# Sys.setenv(AWS_PROFILE = "cities-data-dev",
+#            AWS_DEFAULT_REGION = "us-east-1",
+#            AWS_SDK_LOAD_CONFIG = "1")
+# 
+# s3 <- paws::s3()
+# bucket <- "wri-cities-tcm"
+# aws_http <- "https://wri-cities-tcm.s3.us-east-1.amazonaws.com"
 
-system("aws sso login --profile cities-data-dev")
-Sys.setenv(AWS_PROFILE = "cities-data-dev",
-           AWS_DEFAULT_REGION = "us-east-1",
-           AWS_SDK_LOAD_CONFIG = "1")
-
-s3 <- paws::s3()
-bucket <- "wri-cities-tcm"
-aws_http <- "https://wri-cities-tcm.s3.us-east-1.amazonaws.com"
-
-# Process baseline tile data ----------------------------------------------
-city <- "ZAF-Cape_Town"
-aoi_name <- "urban_extent"
-city_folder <- glue("city_projects/{city}/{aoi_name}")
-
-open_urban_aws_http <- glue("https://wri-cities-heat.s3.us-east-1.amazonaws.com/OpenUrban/{city}")
-
-baseline_name <- "wholecity_start"
-infra_name <- "trees"
-scenario_name <- "pedestrian-achievable-90pctl"
-
-baseline_folder <- glue("{city_folder}/scenarios/baseline/{baseline_name}")
-scenario_folder <- glue("{city_folder}/scenarios/{infra_name}/{scenario_name}")
-
-# Make scenario folder
-ensure_s3_prefix(bucket, scenario_folder)
-
-# Get tile ids
-t <- s3$list_objects_v2(
-  Bucket = bucket,
-  Prefix = baseline_folder
-)
-
-tiles <- list_tiles(bucket, baseline_folder)
-
-aoi <- st_read(glue("{aws_http}/{baseline_folder}/metadata/.qgis_data/urban_extent_boundary.geojson"))
-buffered_tile_grid <- st_read(glue("{aws_http}/{baseline_folder}/metadata/.qgis_data/tile_grid.geojson"))
-tile_grid <- st_read(glue("{aws_http}/{baseline_folder}/metadata/.qgis_data/unbuffered_tile_grid.geojson"))
-
-# TODO: Remove after grid is complete ####
-buffered_tile_grid <- buffered_tile_grid %>% 
-  filter(tile_name %in% tiles)
-tile_grid <- tile_grid %>% 
-  filter(tile_name %in% tiles)
+# # Process baseline tile data ----------------------------------------------
+# city_folder <- glue("city_projects/{city}/{aoi_name}")
+# 
+# open_urban_aws_http <- glue("https://wri-cities-heat.s3.us-east-1.amazonaws.com/OpenUrban/{city}")
+# 
+# baseline_name <- "baseline"
+# 
+# baseline_folder <- glue("{city_folder}/scenarios/baseline/{baseline_name}")
+# scenario_folder <- glue("{city_folder}/scenarios/{infra_name}/{scenario_name}")
+# 
+# # Make scenario folder
+# ensure_s3_prefix(bucket, scenario_folder)
+# 
+# # Get tile ids
+# t <- s3$list_objects_v2(
+#   Bucket = bucket,
+#   Prefix = baseline_folder
+# )
+# 
+# tiles <- list_tiles(bucket, baseline_folder)
+# 
+# if (aoi_name == "urban_extent"){
+#   aoi <- st_read(glue("{aws_http}/{baseline_folder}/metadata/.qgis_data/urban_extent_boundary.geojson"))
+# } else {
+#   aoi <- st_read(aoi_path)
+# }
+# 
+# buffered_tile_grid <- st_read(glue("{aws_http}/{baseline_folder}/metadata/.qgis_data/tile_grid.geojson"))
+# tile_grid <- st_read(glue("{aws_http}/{baseline_folder}/metadata/.qgis_data/unbuffered_tile_grid.geojson"))
 
 
 
@@ -64,14 +68,15 @@ tile_grid <- tile_grid %>%
 
 make_binary_tree_cover <- function(tree_canopy){
   binary_tree <- tree_canopy >= 3
+  return(binary_tree)
 }
 
 # Process tree canopy to trees --------------------------------------------
 
-process_trees <- function(tree_canopy, city_folder, baseline_folder, tile_id){
+process_trees <- function(tree_canopy, city_folder, baseline_folder, t_id){
   
   tile <- tile_grid %>% 
-    filter(tile_id == tile_id)
+    filter(tile_name == t_id)
   
   # Load raster into memory
   tree_canopy[tree_canopy < 3] <- 0
@@ -92,22 +97,22 @@ process_trees <- function(tree_canopy, city_folder, baseline_folder, tile_id){
     st_as_sf() %>% 
     left_join(st_drop_geometry(ttops), by = "treeID") %>% 
     rename(height = Z)  %>% 
-    mutate(tile = tile_id) %>% 
+    mutate(tile = t_id) %>% 
     st_filter(tile_grid, .predicate = st_within)
   
   ttops <- ttops %>% 
     rename(height = Z) %>% 
     st_zm(drop = TRUE, what = "ZM")
   
-  write_s3(ttops, glue("{bucket}/{baseline_folder}/{tile_id}/layers/tree-points.geojson"))
-  write_s3(crowns, glue("{bucket}/{baseline_folder}/{tile_id}/layers/existing-tree-crowns.tif"))
-  write_s3(crown_vectors, glue("{bucket}/{baseline_folder}/{tile_id}/layers/existing-tree-crowns.geojson"))
+  write_s3(ttops, glue("{bucket}/{baseline_folder}/{t_id}/ccl_layers/tree-points.geojson"))
+  write_s3(crowns, glue("{bucket}/{baseline_folder}/{t_id}/ccl_layers/existing-tree-crowns.tif"))
+  write_s3(crown_vectors, glue("{bucket}/{baseline_folder}/{t_id}/ccl_layers/existing-tree-crowns.geojson"))
 }
 
 
 # Create pedestrian area --------------------------------------------------
 
-create_pedestrian_area <- function(lulc){
+create_pedestrian_area <- function(lulc, open_urban_aws_http){
   
   utm <- st_crs(lulc)
   
@@ -133,8 +138,9 @@ create_pedestrian_area <- function(lulc){
   
   road_vectors <- road_vectors %>% 
     select(highway, lanes) %>% 
+    mutate(lanes = as.integer(lanes)) %>% 
     left_join(lanes, by = "highway") %>% 
-    mutate(lanes = coalesce(lanes, avg.lanes))
+    mutate(lanes = coalesce(lanes, avg_lanes))
   
   ped_road_vectors <- road_vectors %>% 
     filter(highway %in% ped_roads_list)
@@ -166,13 +172,13 @@ create_pedestrian_area <- function(lulc){
   
   # Remove roads and water
   pedestrian_area <- pedestrian_area * abs((floor(lulc / 100) %in% c(3, 5)) - 1)
-  
+  return(pedestrian_area)
 }
 
 
 # Create plantable area ---------------------------------------------------
 
-create_plantable_area <- function(lulc, pedestrian_area, binary_tree_cover){
+create_plantable_area <- function(lulc, pedestrian_area, binary_tree_cover, open_urban_aws_http){
   
   utm <- st_crs(lulc)
   
@@ -197,6 +203,12 @@ create_plantable_area <- function(lulc, pedestrian_area, binary_tree_cover){
   road_vectors <- st_read_parquet(glue("{open_urban_aws_http}/roads/roads_all.parquet")) %>% 
     st_transform(utm) %>% 
     st_filter(tile_geom)
+  
+  if (nrow(road_vectors) == 0) {
+    plantable_street <- lulc
+    values(plantable_street) <- 0
+    return(plantable_street)
+  }
   
   # Dissolve road segments 
   dissolved_roads <- st_union(road_vectors)
@@ -234,65 +246,59 @@ create_plantable_area <- function(lulc, pedestrian_area, binary_tree_cover){
   
   # Remove areas of existing tree cover
   plantable_street <- plantable_street * (binary_tree_cover < 1)
+  return(plantable_street)
 }
 
 
 # Baseline tree processing per tile ---------------------------------------
 
 # baseline folder is the output of the baseline CTCM run
-baseline_processing <- function(tile_id){
+baseline_processing <- function(t_id){
+  print(t_id)
   
   ensure_s3_prefix(
     bucket,
-    glue("{baseline_folder}/{tile_id}/layers/")
+    glue("{baseline_folder}/{t_id}/ccl_layers/")
   )
-  
+
   # Load data
-  tree_canopy <- rast(glue("{aws_http}/{baseline_folder}/{tile_id}/raster_files/cif_tree_canopy.tif"))
-  lulc <- rast(glue("{aws_http}/{baseline_folder}/{tile_id}/raster_files/cif_open_urban.tif"))
-  
+  tree_canopy <- rast(glue("{aws_http}/{baseline_folder}/{t_id}/raster_files/cif_tree_canopy.tif"))
+  lulc <- rast(glue("{aws_http}/{baseline_folder}/{t_id}/raster_files/cif_open_urban.tif"))
+
   # Create and save binary tree cover
   binary_tree_cover <- make_binary_tree_cover(tree_canopy)
-  write_s3(binary_tree_cover, glue("{bucket}/{baseline_folder}/{tile_id}/layers/tree-cover.tif"))
-  
-  # Only process trees if there are any nonzero pixels
-  # if (global(binary_tree_cover, "max", na.rm = TRUE)[1,1] > 0) {
-  #   process_trees(tree_canopy, city_folder, baseline_folder, tile)
-  # } else {
-  #   return(invisible(NULL))
-  # }
-  process_trees(tree_canopy, city_folder, baseline_folder, tile_id = tile_id)
-  
+  write_s3(binary_tree_cover, glue("{bucket}/{baseline_folder}/{t_id}/ccl_layers/tree-cover.tif"))
+
+  process_trees(tree_canopy, city_folder, baseline_folder, t_id)
+
   # Create pedestrian area
-  pedestrian_area <- create_pedestrian_area(lulc)
-  write_s3(pedestrian_area, glue("{bucket}/{baseline_folder}/{tile_id}/layers/pedestrian-areas.tif"))
-  
+  pedestrian_area <- create_pedestrian_area(lulc, open_urban_aws_http)
+  write_s3(pedestrian_area, glue("{bucket}/{baseline_folder}/{t_id}/ccl_layers/pedestrian-areas.tif"))
+
   # Create plantable area
-  plantable_area <- create_plantable_area(lulc, pedestrian_area, binary_tree_cover)
-  
+  plantable_area <- create_plantable_area(lulc, pedestrian_area, binary_tree_cover, open_urban_aws_http)
+
   # dir.create(here(scenario_folder, tile), showWarnings = FALSE, recursive = TRUE)
   ensure_s3_prefix(
     bucket,
-    glue("{scenario_folder}/{tile_id}/layers/")
+    glue("{scenario_folder}/{t_id}/ccl_layers/")
   )
-  write_s3(plantable_area, glue("{bucket}/{scenario_folder}/{tile_id}/layers/plantable-areas.tif"))
-  
-  # Create available points to place trees
-  available_pts <- as.points(subst(plantable_area, from = 0, to = NA)) %>% 
-    st_as_sf() %>% 
-    mutate(pt_id = row_number())
-  
-  write_s3(available_pts, glue("{bucket}/{scenario_folder}/{tile_id}/layers/plantable-points.geojson"))
-}
+  write_s3(plantable_area, glue("{bucket}/{scenario_folder}/{t_id}/ccl_layers/plantable-areas.tif"))
 
-map(tiles, baseline_processing)
+  # Create available points to place trees
+  available_pts <- as.points(subst(plantable_area, from = 0, to = NA)) %>%
+    st_as_sf() %>%
+    mutate(pt_id = row_number())
+
+  write_s3(available_pts, glue("{bucket}/{scenario_folder}/{t_id}/ccl_layers/plantable-points.geojson"))
+}
 
 
 # Create city-wide tree population ----------------------------------------
 
 create_tree_population <- function(tiles){
   
-  tree_paths <- glue("{aws_http}/{baseline_folder}/{tiles}/layers/tree-points.geojson")
+  tree_paths <- glue("{aws_http}/{baseline_folder}/{tiles}/ccl_layers/tree-points.geojson")
   
   # Safe reader (returns NULL if file missing)
   safe_read <- possibly(~ st_read(.x, quiet = TRUE), otherwise = NULL)
@@ -311,7 +317,7 @@ create_tree_population <- function(tiles){
   )
   
   # Filter crowns to only those with heights in tree structure
-  crown_paths <- paste(aws_http, baseline_folder, tiles, "layers", "existing-tree-crowns.geojson", sep = "/")
+  crown_paths <- paste(aws_http, baseline_folder, tiles, "ccl_layers", "existing-tree-crowns.geojson", sep = "/")
   crowns <- map(crown_paths, ~ {
     x <- safe_read(.x)
     if (!is.null(x)) {
@@ -323,65 +329,16 @@ create_tree_population <- function(tiles){
     compact() %>%
     bind_rows()
   
-  ensure_s3_prefix(bucket, glue("{bucket}/{scenario_folder}/tree-population"))
+  ensure_s3_prefix(bucket, glue("{scenario_folder}/tree-population/"))
   
   write_s3(trees, glue("{bucket}/{scenario_folder}/tree-population/trees.geojson"))
   write_s3(crowns, glue("{bucket}/{scenario_folder}/tree-population/crowns.geojson"))
   write_s3(tree_structure, glue("{bucket}/{scenario_folder}/tree-population/tree-structure.csv"))
 }
 
-create_tree_population(tiles)
 
+# Plant new trees ---------------------------------------------------------
 
-# Create city-wide grid ---------------------------------------------------
-
-
-
-# Achievable potential
-tree_grid_path <- glue("{open_urban_aws_http}/scenarios/street-trees/{city}-street-tree-pct-1km-grid.csv")
-
-# Get percentile value
-ped_area_tree_dist <- read_csv(tree_grid_path)
-target_coverage <- quantile(ped_area_tree_dist$`pct-tree`, 0.9, 
-                            names = FALSE, na.rm = TRUE)
-min_dist <- 5
-
-# Create a grid over the tile geometery to iterate over for creating trees
-# intersect with AOI so only areas within the aoi are planted
-# aoi <- st_read(here("data", city_folder, "aoi.geojson"))
-
-aoi_grid <- aoi %>% 
-  st_make_grid(cellsize = c(100, 100), square = TRUE, what = "polygons") %>% 
-  st_sf() %>% 
-  st_intersection(aoi) %>% 
-  select(geometry) %>% 
-  mutate(ID = row_number())
-
-# TODO: Remove this once full grid is available #### 
-aoi_grid <- aoi_grid %>% 
-  st_filter(buffered_tile_grid) %>% 
-  st_filter(tile_grid) 
-  
-
-# Copy existing tree canopy tiles into scenario folder to be updated
-tree_canopy_paths <- glue("{baseline_folder}/{tiles}/raster_files/cif_tree_canopy.tif")
-scenario_tree_canopy_paths <- glue("{scenario_folder}/{tiles}/raster_files/tree_canopy.tif")
-
-map(glue("{scenario_folder}/{tiles}/raster_files/"), ~ ensure_s3_prefix(bucket, .x))
-s3_copy_vec(from = tree_canopy_paths, to = scenario_tree_canopy_paths, bucket, overwrite = TRUE)
-
-# Load tree population data
-trees <- st_read(glue("{aws_http}/{scenario_folder}/tree-population/trees.geojson"))
-crowns <- st_read(glue("{aws_http}/{scenario_folder}/tree-population/crowns.geojson"))
-tree_structure <- read_csv(glue("{aws_http}/{scenario_folder}/tree-population/tree-structure.csv"))
-
-
-# Create an empty sf to store new trees
-new_trees <- st_sf(
-  height = numeric(0),
-  type   = character(0),
-  geometry = st_sfc(crs = st_crs(aoi))
-)
 
 # Gridcell is grid geometry
 plant_in_gridcell <- function(grid_index, target_coverage, min_dist){
@@ -417,7 +374,7 @@ plant_in_gridcell <- function(grid_index, target_coverage, min_dist){
     crop(gridcell)
   
   # Plantable area for the gridcell
-  plantable_paths <- glue("{aws_http}/{scenario_folder}/{unbuffered_tile_names}/layers/plantable-areas.tif")
+  plantable_paths <- glue("{aws_http}/{scenario_folder}/{unbuffered_tile_names}/ccl_layers/plantable-areas.tif")
   plantable_area <- load_and_merge(plantable_paths) %>% 
     crop(gridcell) %>% 
     subst(from = 0, to = NA)
@@ -427,7 +384,7 @@ plant_in_gridcell <- function(grid_index, target_coverage, min_dist){
     replace_na(0)
   
   # Pedestrian area for the gridcell
-  ped_area_paths <- glue("{aws_http}/{baseline_folder}/{unbuffered_tile_names}/layers/pedestrian-areas.tif")
+  ped_area_paths <- glue("{aws_http}/{baseline_folder}/{unbuffered_tile_names}/ccl_layers/pedestrian-areas.tif")
   ped_area <- load_and_merge(ped_area_paths) %>% 
     crop(gridcell) %>% 
     subst(from = 0, to = NA)
@@ -437,7 +394,7 @@ plant_in_gridcell <- function(grid_index, target_coverage, min_dist){
     replace_na(0)
   
   # Current tree cover
-  current_tree_cover_paths <- glue("{aws_http}/{baseline_folder}/{unbuffered_tile_names}/layers/tree-cover.tif")
+  current_tree_cover_paths <- glue("{aws_http}/{baseline_folder}/{unbuffered_tile_names}/ccl_layers/tree-cover.tif")
   current_tree_cover <- load_and_merge(current_tree_cover_paths) %>% 
     crop(gridcell) %>% 
     subst(from = 0, to = NA) %>% 
@@ -510,7 +467,7 @@ plant_in_gridcell <- function(grid_index, target_coverage, min_dist){
     available_pts <- available_pts %>% filter(pt_id != candidate$pt_id)
     
     # Rasterize crown
-    crown_pixels <- rast(glue("{aws_http}/{baseline_folder}/{crown_geom$tile}/layers/existing-tree-crowns.tif")) %>% 
+    crown_pixels <- rast(glue("{aws_http}/{baseline_folder}/{crown_geom$tile}/ccl_layers/existing-tree-crowns.tif")) %>% 
       crop(crown_geom)
     crown_pixels <- crown_pixels == crown_geom$treeID
     crown_pixels <- subst(crown_pixels, from = 0, to = NA)
@@ -553,6 +510,8 @@ plant_in_gridcell <- function(grid_index, target_coverage, min_dist){
   
   # Update tree canopy for all intersecting buffered tree canopy layers
   update_paths <- glue("{aws_http}/{scenario_folder}/{buffered_tile_names}/raster_files/tree_canopy.tif")
+  ensure_s3_prefix(update_paths)
+  
   for (p in update_paths) {
     r <- rast(p)
     # Make canopy_height_gridcell the same extent as r
@@ -567,21 +526,60 @@ plant_in_gridcell <- function(grid_index, target_coverage, min_dist){
   }
   
   return(new_tree_pts)
-
+  
 }
 
-t0 <- Sys.time()
+# Create city-wide grid ---------------------------------------------------
+
+map(tiles, baseline_processing)
+create_tree_population(tiles)
+
+# Achievable potential
+tree_grid_path <- glue("{open_urban_aws_http}/scenarios/street-trees/{city}-street-tree-pct-1km-grid.csv")
+
+# Get percentile value
+ped_area_tree_dist <- read_csv(tree_grid_path)
+target_coverage <- quantile(ped_area_tree_dist$`pct-tree`, 0.9, 
+                            names = FALSE, na.rm = TRUE)
+min_dist <- 5
+
+# Create a grid over the tile geometery to iterate over for creating trees
+# intersect with AOI so only areas within the aoi are planted
+aoi <- st_read(aoi_path)
+
+aoi_grid <- aoi %>% 
+  st_make_grid(cellsize = c(100, 100), square = TRUE, what = "polygons") %>% 
+  st_sf() %>% 
+  st_intersection(aoi) %>% 
+  select(geometry) %>% 
+  mutate(ID = row_number())
+
+# Copy existing tree canopy tiles into scenario folder to be updated
+tree_canopy_paths <- glue("{baseline_folder}/{tiles}/raster_files/cif_tree_canopy.tif")
+scenario_tree_canopy_paths <- glue("{scenario_folder}/{tiles}/raster_files/tree_canopy.tif")
+
+map(glue("{scenario_folder}/{tiles}/raster_files/"), ~ ensure_s3_prefix(bucket, .x))
+s3_copy_vec(from = tree_canopy_paths, to = scenario_tree_canopy_paths, bucket, overwrite = TRUE)
+
+# Load tree population data
+trees <- st_read(glue("{aws_http}/{scenario_folder}/tree-population/trees.geojson"))
+crowns <- st_read(glue("{aws_http}/{scenario_folder}/tree-population/crowns.geojson"))
+tree_structure <- read_csv(glue("{aws_http}/{scenario_folder}/tree-population/tree-structure.csv"))
+
+# Create an empty sf to store new trees
+new_trees <- st_sf(
+  height = numeric(0),
+  type   = character(0),
+  geometry = st_sfc(crs = st_crs(aoi))
+)
+
 new_trees <- map(aoi_grid$ID, 
                  ~ plant_in_gridcell(.x, target_coverage = target_coverage, min_dist = min_dist)) %>% 
   compact() %>% 
   bind_rows() %>% 
   select(height, type)
 
-
 # Write new tree points
 write_s3(new_trees, glue("{bucket}/{scenario_folder}/new-tree-points.geojson"))
 
-elapsed <- Sys.time() - t0
-cat(sprintf("Elapsed: %.2f minutes (%.1f seconds)\n",
-            as.numeric(elapsed, "mins"),
-            as.numeric(elapsed, "secs")))
+

e9686f5ab94927f479a2ff8da125184df2335b21
Elizabeth Jane Wesley <elizabeth.wesley@wri.org>
2025-12-23 08:06:15 -0500

    updated source path

--- tiling-scripts/download-tiles-from-s3.r
+++ tiling-scripts/download-tiles-from-s3.r
@@ -1,43 +1,56 @@
 #!/usr/bin/env Rscript
 
-# args <- commandArgs(trailingOnly = TRUE)
-# aoi_path <- args[1]
-# city     <- args[2]
-# infra    <- args[3]
-# scenario <- args[4]
-# 
-# if (any(is.na(c(aoi_path, city, infra, scenario)))) {
-#   stop("Usage: get_tiles_and_copy.R <aoi_path> <city_name> <infra> <scenario>")
-# }
 
-list_tiles <- function(baseline_path, profile = "cities-data-dev") {
-  lines <- system2(
-    "aws",
-    c("s3", "ls", baseline_path, "--profile", profile),
-    stdout = TRUE,
-    stderr = TRUE
-  )
-  
-  tile_lines <- grep("^[[:space:]]*PRE[[:space:]]+tile_[0-9]+/", lines, value = TRUE)
-  sub("^[[:space:]]*PRE[[:space:]]+(tile_[0-9]+)/$", "\\1", tile_lines)
+# Run example -------------------------------------------------------------
+
+
+# Rscript download-tiles-from-s3.r \
+#   "https://wri-cities-heat.s3.us-east-1.amazonaws.com/ZAF-Durban/ZAF-Durban__inner_city_lap.geojson" \
+#   "inner_city_lap" \
+#   "ZAF-Durban" \
+#   "trees" \
+#   "pedestrian-achievable-90pctl" \
+#   FALSE \
+#   TRUE
+
+
+# -------------------------------------------------------------------------
+
+
+args <- commandArgs(trailingOnly = TRUE)
+aoi_path <-       args[1]
+aoi_name <-       args[2]
+city     <-       args[3]
+infra    <-       args[4]
+scenario <-       args[5]
+s3_copy <-        args[6]
+local_download <- args[7]
+
+
+if (any(is.na(c(aoi_path, aoi_name, city, infra, scenario)))) {
+  stop("Usage: get_tiles_and_copy.R <aoi_path> <aoi_name> <city> <infra> <scenario>")
 }
 
-download_tiles <- function(aoi, aoi_name, city, infra, scenario, from_urban_extent = TRUE){
+
+source(file.path("tiling-scripts", "utils.R"))
+
+download_tiles <- function(aoi_path, aoi_name, city, infra, scenario, from_urban_extent = TRUE, s3_copy = TRUE, local_download = TRUE){
   
   library(sf)
   library(glue)
   
   profile  <- "cities-data-dev"
   bucket <- "wri-cities-tcm"
+  
   # # 1) Load AOI polygon
-  # aoi <- st_read(aoi_path, quiet = TRUE)
+  aoi <- st_read(aoi_path, quiet = TRUE)
   out_dir <- file.path("~/CTCM_data_setup", paste(city, scenario, sep = "_"))
   dir.create(out_dir, recursive = TRUE, showWarnings = FALSE)
   
   # 2) Load city grid geojson from AWS (public URL)
   if (from_urban_extent){
     grid_url <- glue(
-      "https://wri-cities-tcm.s3.us-east-1.amazonaws.com/city_projects/{city}/urban_extent/scenarios/baseline/baseline/metadata/.qgis_data/unbuffered_tile_grid.geojson",
+      "https://wri-cities-tcm.s3.us-east-1.amazonaws.com/city_projects/{city}/urban_extent/scenarios/{infra}/{scenario}/metadata/.qgis_data/unbuffered_tile_grid.geojson",
     )
     grid <- st_read(grid_url, quiet = TRUE)
     
@@ -47,34 +60,53 @@ download_tiles <- function(aoi, aoi_name, city, infra, scenario, from_urban_exte
     tiles <- grid$tile_name
     
     for (t in tiles) {
-      s3_src  <- glue("s3://wri-cities-tcm/city_projects/{city}/urban_extent/scenarios/baseline/baseline/{t}/raster_files")
-      local_d <- file.path(out_dir, "primary_data", "raster_files", t)
-      dir.create(local_d, recursive = FALSE, showWarnings = FALSE)
+      s3_src  <- glue("s3://wri-cities-tcm/city_projects/{city}/urban_extent/scenarios/{infra}/{scenario}/{t}/raster_files")
       
-      cmd <- sprintf("aws s3 cp %s %s --recursive --no-sign-request",
-                     shQuote(s3_src), shQuote(local_d))
-      cat("\n", cmd, "\n", sep = "")
-      system(cmd)
+      if (isTRUE(local_download)){
+        local_d <- file.path(out_dir, "primary_data", "raster_files", t)
+        dir.create(local_d, recursive = FALSE, showWarnings = FALSE)
+        
+        cmd <- sprintf("aws s3 cp %s %s --recursive --no-sign-request",
+                       shQuote(s3_src), shQuote(local_d))
+        cat("\n", cmd, "\n", sep = "")
+        system(cmd)
+      }
+      
+      if (isTRUE(s3_copy)) {
+        s3_dst <- glue("s3://wri-cities-tcm/city_projects/{city}/{aoi_name}/scenarios/{infra}/{scenario}/{t}/raster_files")
+        
+        cmd_up <- sprintf(
+          "aws s3 cp %s %s --recursive --profile %s",
+          shQuote(local_d), shQuote(s3_dst), shQuote(profile)
+        )
+        cat("\n", cmd_up, "\n", sep = "")
+        system(cmd_up)
+      }
     }
     
   } else {
     
-    baseline_path <- glue(
-      "s3://wri-cities-tcm/city_projects/{city}/{aoi_name}/scenarios/baseline/baseline/"
+    data_path <- glue(
+      "s3://wri-cities-tcm/city_projects/{city}/{aoi_name}/scenarios/{infra}/{scenario}/"
     )
     
-    tiles <- list_tiles(baseline_path)
+    tiles <- list_tiles(data_path)
     
     for (t in tiles) {
-      s3_src  <- glue("{baseline_path}/{t}/raster_files")
-      local_d <- file.path(out_dir, "primary_data", "raster_files", t)
-      dir.create(local_d, recursive = FALSE, showWarnings = FALSE)
+      s3_src  <- glue("{data_path}/{t}/raster_files")
       
-      cmd <- sprintf("aws s3 cp %s %s --recursive --no-sign-request",
-                     shQuote(s3_src), shQuote(local_d))
-      cat("\n", cmd, "\n", sep = "")
-      system(cmd)
+      if (isTRUE(local_download)){
+        local_d <- file.path(out_dir, "primary_data", "raster_files", t)
+        dir.create(local_d, recursive = FALSE, showWarnings = FALSE)
+        
+        cmd <- sprintf("aws s3 cp %s %s --recursive --no-sign-request",
+                       shQuote(s3_src), shQuote(local_d))
+        cat("\n", cmd, "\n", sep = "")
+        system(cmd)
+      }
     }
   }
   
 }
+
+

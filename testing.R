cc6e834ced6e8291ae7ca0b27a94c3e281c49966
Elizabeth Jane Wesley <elizabeth.wesley@wri.org>
2026-01-30 10:41:42 -0500

    updated testing script to run ccl layers and calc metrics for cool roofs

--- testing.R
+++ testing.R
@@ -1,41 +1,92 @@
-cities <- c("BRA-Teresina", "BRA-Recife", "BRA-Fortaleza", "BRA-Florianopolis", "BRA-Campinas")
+# system("aws sso login --profile cities-data-dev")
+# Sys.setenv(AWS_PROFILE = "cities-data-dev",
+#            AWS_DEFAULT_REGION = "us-east-1",
+#            AWS_SDK_LOAD_CONFIG = "1")
+
+library(glue)
+library(tidyverse)
+library(here)
+library(sf)
+library(terra)
+
 source(here("tiling-scripts", "metrics-functions.R"))
+source(here("tiling-scripts", "utils.R"))
+source(here("tiling-scripts", "post-processing-functions.R"))
+
+cities <- c("BRA-Recife", "BRA-Fortaleza", "BRA-Florianopolis", "BRA-Campinas", "BRA-Teresina")
+
 
 for (city in cities){
+    print(city)
+    city <- "MEX-Hermosillo"
+    aoi_name <- "urban_extent"
+    
+    bucket   <- "wri-cities-tcm"
+    aws_http <- "https://wri-cities-tcm.s3.us-east-1.amazonaws.com"
+    s3 <- paws::s3()
+    
+    country <- strsplit(city, "-")[[1]][1]
+    
+    city_folder     <- file.path("city_projects", city, aoi_name)
+    baseline_folder <- file.path(city_folder, "scenarios", "baseline", "baseline")
+    
+    aoi_path <- glue("https://wri-cities-tcm.s3.us-east-1.amazonaws.com/city_projects/{city}/{aoi_name}/scenarios/baseline/baseline/aoi__baseline__baseline.geojson")
+    
+    tiles_s3 <- list_tiles(paste0("s3://", bucket, "/", baseline_folder))
+    
+    tile_grid <- st_read(
+      paste0(aws_http, "/", baseline_folder, "/metadata/.qgis_data/unbuffered_tile_grid.geojson"),
+      quiet = TRUE
+    ) |>
+      filter(tile_name %in% tiles_s3)
+    
+    buffered_tile_grid <- st_read(
+      paste0(aws_http, "/", baseline_folder, "/metadata/.qgis_data/tile_grid.geojson"),
+      quiet = TRUE
+    ) %>%
+      dplyr::filter(tile_name %in% tiles_s3)
+    
+    utm <- st_crs(tile_grid)
+    
+    aoi <- st_read(aoi_path, quiet = TRUE) |>
+      st_transform(st_crs(utm))
+    
+    tile_grid_aoi <- tile_grid |> 
+      st_filter(aoi) 
+    tiles_aoi <- tile_grid_aoi$tile_name
+    
+    infra <- "cool-roofs"
+    scenario <- "all-buildings"
+    
+    process_tcm_layers(baseline_folder, infra, scenario, scenario_folder)
+    calc_cool_roofs_metrics(city, aoi_name, tiles_aoi, scenario)
+    # for (scenario in c("baseline", "pedestrian-achievable-90pctl")){
+    # 
+    # if (scenario == "pedestrian-achievable-90pctl"){
+    #   infra = "trees"
+    # } else if (scenario == "baseline"){
+    #   infra = "baseline"
+    # }
+    
+    # scenario_folder <- file.path(city_folder, "scenarios", infra, scenario)
+    # 
+    # retain_only_tiles_s3(city,
+    #                      aoi_name,
+    #                      infra,
+    #                      scenario,
+    #                      tiles_aoi,
+    #                      dry_run = FALSE)
+    # 
+    # process_tcm_layers(baseline_folder, infra, scenario, scenario_folder)
+    ###########################
+    
+  }
   
-  city <- "ZAF-Durban"
-  aoi_name <- "corridors_of_excellence"
   
-  bucket   <- "wri-cities-tcm"
-  aws_http <- "https://wri-cities-tcm.s3.us-east-1.amazonaws.com"
-  s3 <- paws::s3()
-  
-  city_folder     <- file.path("city_projects", city, aoi_name)
-  baseline_folder <- file.path(city_folder, "scenarios", "baseline", "baseline")
-  
-  aoi_path <- glue("https://wri-cities-tcm.s3.us-east-1.amazonaws.com/city_projects/{city}/{aoi_name}/scenarios/baseline/baseline/aoi__baseline__baseline.geojson")
-  
-  tiles_s3 <- list_tiles(paste0("s3://", bucket, "/", baseline_folder))
-  
-  tile_grid <- st_read(
-    paste0(aws_http, "/", baseline_folder, "/metadata/.qgis_data/unbuffered_tile_grid.geojson"),
-    quiet = TRUE
-  ) |>
-    filter(tile_name %in% tiles_s3)
-  
-  utm <- st_crs(tile_grid)
-  
-  aoi <- st_read(aoi_path, quiet = TRUE) |>
-    st_transform(st_crs(utm))
-  
-  tile_grid_aoi <- tile_grid |> 
-    st_filter(aoi) 
-  tiles_aoi <- tile_grid_aoi$tile_name
-  
-  ###########################
-  
-  baseline_metrics(city, aoi_name, tiles_aoi)
-  calc_street_tree_metrics(city, aoi_name, tiles_aoi, scenario = "custom-n_trees_uniform")
+  # baseline_metrics(city, aoi_name, tiles_aoi)
+  # calc_street_tree_metrics(city, aoi_name, tiles_aoi, scenario = "custom-n_trees_uniform")
   
   
-}
+# }
+
+
